90596390a920952334a65ea07f149c00887b051c
PitchBlackNights <pitchblacknights@proton.me>
2025-12-12 00:45:28 -0600

    Added more build.rs configuration

--- build.rs
+++ build.rs
@@ -1,3 +1,9 @@
+use oxidizemc_fabric::build::FABRICMC_MOD_LOADER;
+use oxidizemc::build::OxidizeMCBuild;
+
 fn main() {
-    oxidizemc_fabric::build::configure();
+    OxidizeMCBuild::new()
+        .java_package("com.github.pitchblacknights.oxidizemc")
+        .mod_loader(FABRICMC_MOD_LOADER)
+        .finish();
 }
